eb0e1ddf7c3301b6aa0d2106ea99edc129a5d1be
Bipin <bk3284488@gmail.com>
2025-01-04 16:06:03 +0530

    user creation api done

--- app/page.js
+++ app/page.js
@@ -1,4 +1,25 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import { api } from "../convex/_generated/api";
+import { useEffect } from "react";
+
 export default function Home() {
+	const { user } = useUser();
+	const createUser = useMutation(api.user.createUser);
+
+	useEffect(() => {
+		user && checkUser();
+	}, [user]);
+
+	const checkUser = async () => {
+		const result = await createUser({
+			email: user?.primaryEmailAddress?.emailAddress,
+			userName: user?.fullName,
+			imageUrl: user?.imageUrl,
+		});
+		console.log(result);
+	};
 	return (
 		<div>
 			<h1>Hello from Next js</h1>
